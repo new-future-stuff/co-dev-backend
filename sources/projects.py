a3fe7bb4bbfa3fb86813a925c4c69d46d36b643e
megahomyak <g.megahomyak@gmail.com>
2023-02-16 21:42:11 +0500

    Added a missing type hint

--- sources/projects.py
+++ sources/projects.py
@@ -2,7 +2,7 @@ from typing import List
 from sources import models
 
 
-async def create_project(name: str, required_skills: List[models.Skill], creator) -> models.Project:
+async def create_project(name: str, required_skills: List[models.Skill], creator: models.User) -> models.Project:
     return await models.Project.create(
         creator=creator,
         required_skills=required_skills,
