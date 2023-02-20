918a3c2b16d662a52fe027126120ffd15ed02065
megahomyak <g.megahomyak@gmail.com>
2023-02-20 10:39:57 +0500

    Let's begin!

---
+++ sources/server.py
@@ -0,0 +1,16 @@
+from fastapi import FastAPI
+from fastapi.responses import FileResponse
+from fastapi.staticfiles import StaticFiles
+from pathlib import Path
+
+
+STATIC = Path("../static")
+
+
+app = FastAPI()
+app.mount("/static", StaticFiles(directory=STATIC), name="static")
+
+
+@app.get("/")
+async def main_page():
+    return FileResponse(STATIC / "main_page.html")
