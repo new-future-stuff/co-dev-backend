62c84d87ee0c1b3704e0a5ecc8097a4f81944f24
megahomyak <g.megahomyak@gmail.com>
2023-03-04 20:24:56 +0500

    Checkpoint! I forgot what I was doing back then...

--- sources/api.py
+++ sources/api.py
@@ -1,10 +1,72 @@
-from fastapi import APIRouter
-from pydantic import BaseModel
+from fastapi import APIRouter, HTTPException, Request, status, Response, Form
+from secrets import token_urlsafe
+
+from fastapi.responses import RedirectResponse
 
 
 router = APIRouter()
 
 
+def _add_user(password, token, email):
+    users[email] = {"password": password, "token": token, "projects": [], "skills": []}
+
+
+users = {}
+_add_user("pass", "tok", "mail")
+
+last_project_id = 0
+
+
+def get_user(request: Request, *, required: bool):
+    try:
+        token = request.cookies["token"]
+    except KeyError:
+        pass
+    else:
+        for _email, user in users.items():
+            if user["token"] == token:
+                return user
+    if required:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED)
+
+
 @router.post("/authenticate")
-async def authenticate(email: str, password: str):
-    return await 
+async def authenticate(email: str = Form(), password: str = Form()):
+    try:
+        user = users[email]
+    except KeyError:
+        pass
+    else:
+        if user["password"] == password:
+            token = user["token"]
+            response = RedirectResponse(url="/", status_code=status.HTTP_303_SEE_OTHER)
+            response.set_cookie("token", token)
+            return response
+    raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST)
+
+
+@router.post("/register")
+async def register(response: Response, email: str = Form(), password: str = Form()):
+    if email in users:
+        return HTTPException(status_code=status.HTTP_409_CONFLICT)
+    else:
+        token = token_urlsafe(128)
+        _add_user(password, token, email)
+        response.set_cookie("token", token)
+        return RedirectResponse(url="/", status_code=status.HTTP_303_SEE_OTHER)
+
+
+@router.post("/set_skills")
+async def set_skills(request: Request, skills: str = Form()):
+    user = get_user(request, required=True)
+    skills: list = skills.split()
+    user["skills"] = skills
+    return RedirectResponse(url="/", status_code=status.HTTP_303_SEE_OTHER)
+
+
+@router.post("/create_project")
+async def create_project(request: Request, title: str):
+    user = get_user(request, required=True)
+    user["projects"].append({"id": last_project_id, "title": title})
+    last_project_id += 1
+    return RedirectResponse(url="/projects", status_code=status.HTTP_303_SEE_OTHER)
--- sources/server.py
+++ sources/server.py
@@ -1,16 +1,44 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from fastapi.responses import FileResponse
 from fastapi.staticfiles import StaticFiles
 from pathlib import Path
+from fastapi.templating import Jinja2Templates
+import api
 
 
 STATIC = Path("../static")
 
+templates = Jinja2Templates("../templates")
+
 
 app = FastAPI()
 app.mount("/static", StaticFiles(directory=STATIC), name="static")
+app.include_router(api.router, prefix="/api")
 
 
 @app.get("/")
-async def main_page():
-    return FileResponse(STATIC / "main.html")
+async def main_page(request: Request):
+    user = api.get_user(request, required=False)
+    return templates.TemplateResponse("main.jinja2", context={"user": user, "request": request})
+
+
+@app.get("/registration")
+async def registration():
+    return FileResponse(STATIC / "registration.html")
+
+
+@app.get("/authentication")
+async def authentication():
+    return FileResponse(STATIC / "authentication.html")
+
+
+@app.get("/skills")
+async def skills(request: Request):
+    user = api.get_user(request, required=False)
+    return templates.TemplateResponse("skills.jinja2", context={"user": user, "request": request})
+
+
+@app.get("/projects")
+async def projects(request: Request):
+    user = api.get_user(request, required=True)
+    return templates.TemplateResponse("projects.jinja2", context={"user": user, "request": request})
